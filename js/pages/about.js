84fee93f3647e8d8571dbeb82628fec569569618
Cezary Bąk <sekkabak@gmail.com>
2020-08-14 17:25:01 +0200

    Update about.js

--- js/pages/about.js
+++ js/pages/about.js
@@ -4,7 +4,8 @@ const aboutPage = Vue.component('about-page', {
             <p>
                 Hello! My name is Cezary Bąk. 
                 I am a Cybersecurity B.E. student at  MUT(MILITARY UNIVERSITY OF TECHNOLOGY) in Warsaw. 
-                I love new technologies, especially discover how things are developed. 
+                I am passionate about technology, its constant development and progress. 
+                It helps me to work more effectively with new technologies and makes it much easier for me to see my own progress and success. 
                 My favorite language is Python because he let me automate many things in my life. 
                 Also, allow me to quickly write complex programs.
             </p>
